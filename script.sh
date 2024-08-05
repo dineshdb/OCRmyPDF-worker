2c2b7e242754e0a03d180c8df9cec14c9bd60c4d
dineshdb <dineshdb@dbhattarai.info.np>
2024-08-05 17:59:13 +0545

    feat: produce only new output

--- script.sh
+++ script.sh
@@ -8,11 +8,16 @@ cd $SOURCE_DIR || exit 1
 # using find to search for directory and execute the bash commands
 find . -type f -iname '*.pdf' -exec bash -c '
 	basename=$(basename "$0" .pdf)
+	target_file="$TARGET_DIR/$basename.pdf"
+	if [ ! -f $target_file ]; then
+		echo "Processing $basename.pdf"
 
-	echo "Processing $basename.pdf"
+		# save to intermediate file system so that failed jobs
+		# wont pollute the output
+		ocrmypdf "$basename.pdf" "/var/run/$basename.ocr.pdf"
+		mv "/var/run/$basename.ocr.pdf" "$target_file"
 
-	ocrmypdf "$basename.pdf" "$basename.ocr.pdf"
-	mv "$basename.ocr.pdf" "$TARGET_DIR/$basename.pdf"
+		echo "Processed $basename.pdf"
+	fi
 
-	echo "Processed $basename.pdf"
 ' {} \;
