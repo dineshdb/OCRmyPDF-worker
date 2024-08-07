95170228005467e9f26d05a9dade6f3e5085ab62
dineshdb <dineshdb@dbhattarai.info.np>
2024-08-07 15:49:39 +0545

    feat: add logs

--- script.sh
+++ script.sh
@@ -5,10 +5,13 @@ TARGET_DIR="${TARGET_DIR:-/var/lib/target}"
 
 cd $SOURCE_DIR || exit 1
 
+echo "Watching $SOURCE_DIR for new PDF files..."
+
 # using find to search for directory and execute the bash commands
 find . -type f -iname '*.pdf' -exec bash -c '
 	basename=$(basename "$0" .pdf)
 	target_file="$TARGET_DIR/$basename.pdf"
+	echo "Found $target_file"
 	if [ ! -f $target_file ]; then
 		echo "Processing $basename.pdf to $target_file"
 		ocrmypdf "$basename.pdf" "$target_file"
